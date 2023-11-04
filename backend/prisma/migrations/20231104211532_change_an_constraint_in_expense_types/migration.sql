941052e78e8df8ccf102305999f5f98a415e7d2e
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-11-04 18:17:59 -0300

    feat: Finish the prisma config

---
+++ backend/prisma/migrations/20231104211532_change_an_constraint_in_expense_types/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `expense_types` MODIFY `description` VARCHAR(191) NULL;
--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -4,7 +4,71 @@ const client = new PrismaClient();
 
 const main = async () => {
   await client.role.createMany({
-    data: []
+    data: [
+      {
+        name: 'Dono',
+        description: 'Dono do negócio',
+      },
+      {
+        name: 'Administrador/Desenvolvedor',
+        description: 'Administrador do negócio',
+      },
+      {
+        name: 'Colaborador',
+        description: 'Um colaborador contratado',
+      }
+    ]
+  });
+
+  await client.paymentStatus.createMany({
+    data: [
+      {
+        name: 'Conta em aberto',
+        description: 'Serviço sem nenhum pagamento registrado',
+      },
+      {
+        name: 'Parcialmente pago',
+        description: 'Pago apenas uma parte',
+      },
+      {
+        name: 'Totalmente pago',
+        description: 'Conta totalmente paga',
+      }
+    ]
+  });
+
+  await client.paymentType.createMany({
+    data: [
+      {
+        name: 'PIX',
+        description: 'Uma conta paga utilizando pix',
+      },
+      {
+        name: 'À Vista',
+        description: 'Uma conta paga utilizando dinheiro',
+      },
+      {
+        name: 'Transferência Bancária',
+        description: 'Uma conta paga utilizando transferência bancária',
+      }
+    ]
+  });
+
+  await client.expenseType.createMany({
+    data: [
+      {
+        name: 'Material',
+      },
+      {
+        name: 'Manutenção',
+      },
+      {
+        name: 'Pagamento de conta',
+      },
+      {
+        name: 'Pagamento de empregado ',
+      },
+    ]
   });
 }
 
